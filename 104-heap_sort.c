b94f598206551cd05ac0f7acc5c8f191c02fe99c
magajisuleiman <magajisuleiman1203@gmail.com>
2023-07-25 03:59:52 -0700

    heap

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,75 +1,203 @@
-#include "sort.h"
-
-void swap_ints(int *a, int *b);
-void max_heapify(int *array, size_t size, size_t base, size_t root);
-void heap_sort(int *array, size_t size);
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * max_heapify - Turn a binary tree into a complete binary heap.
- * @array: An array of integers representing a binary tree.
- * @size: The size of the array/tree.
- * @base: The index of the base row of the tree.
- * @root: The root node of the binary tree.
- */
-void max_heapify(int *array, size_t size, size_t base, size_t root)
-{
-	size_t left, right, large;
-
-	left = 2 * root + 1;
-	right = 2 * root + 2;
-	large = root;
-
-	if (left < base && array[left] > array[large])
-		large = left;
-	if (right < base && array[right] > array[large])
-		large = right;
-
-	if (large != root)
-	{
-		swap_ints(array + root, array + large);
-		print_array(array, size);
-		max_heapify(array, size, base, large);
-	}
-}
-
-/**
- * heap_sort - Sort an array of integers in ascending
- *             order using the heap sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
+ * struct listint_s - Doubly linked list node
  *
- * Description: Implements the sift-down heap sort
- * algorithm. Prints the array after each swap.
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
-void heap_sort(int *array, size_t size)
+typedef struct listint_s
 {
-	int i;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
 
-	if (array == NULL || size < 2)
-		return;
+/**
+ * createNode - Function to create a new node for the doubly linked list
+ * @data: Integer value to store in the node
+ * Return: Pointer to the newly created node
+ *
+ * Description: This function creates a new node for the doubly linked list
+ * with the given data value and returns a pointer to the newly created node.
+ */
+listint_t *createNode(int data)
+{
+    listint_t *newNode = (listint_t *)malloc(sizeof(listint_t));
+    if (newNode == NULL)
+        return NULL;
 
-	for (i = (size / 2) - 1; i >= 0; i--)
-		max_heapify(array, size, size, i);
+    newNode->n = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
 
-	for (i = size - 1; i > 0; i--)
-	{
-		swap_ints(array, array + i);
-		print_array(array, size);
-		max_heapify(array, size, i, 0);
-	}
+/**
+ * swap - Function to swap two elements in the doubly linked list
+ * @a: First element to swap
+ * @b: Second element to swap
+ *
+ * Description: This function swaps the values of two elements
+ * in the doubly linked list.
+ */
+void swap(listint_t *a, listint_t *b)
+{
+    int temp = a->n;
+
+    a->n = b->n;
+    b->n = temp;
+}
+
+/**
+ * heap_sort - Function to perform the Heap sort algorithm on the doubly linked list
+ * @head: Pointer to the head of the doubly linked list
+ * @size: Size of the doubly linked list
+ *
+ * Description: This function sorts the doubly linked list using the Heap sort algorithm.
+ * It first builds the heap from the list, then extracts elements one by one to obtain
+ * the sorted list. The doubly linked list will be printed after each swap to demonstrate
+ * the sorting steps.
+ */
+void heap_sort(listint_t **head, size_t size)
+{
+    // Build the heap (rearrange the doubly linked list)
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
+        listint_t *root = *head;
+        for (int j = 0; j < i; j++)
+        {
+            root = root->next;
+        }
+
+        listint_t *largest = root;
+        listint_t *leftChild = root->next;
+        listint_t *rightChild = root->next->next;
+
+        // Compare root with left child
+        if (leftChild != NULL && leftChild->n > largest->n)
+            largest = leftChild;
+
+        // Compare largest with right child
+        if (rightChild != NULL && rightChild->n > largest->n)
+            largest = rightChild;
+
+        // If the largest element is not the root, then swap and continue sifting down
+        if (largest != root)
+        {
+            swap(root, largest);
+            // Print the doubly linked list after each swap
+            listint_t *current = *head;
+            while (current != NULL)
+            {
+                printf("%d ", current->n);
+                current = current->next;
+            }
+            printf("\n");
+        }
+    }
+
+    // Extract elements from the heap one by one
+    listint_t *last = *head;
+    while (last->next != NULL)
+    {
+        last = last->next;
+    }
+    for (int i = size - 1; i > 0; i--)
+    {
+        // Move the current root to the end
+        swap(*head, last);
+        // Print the doubly linked list after each swap
+        listint_t *current = *head;
+        while (current != NULL)
+        {
+            printf("%d ", current->n);
+            current = current->next;
+        }
+        printf("\n");
+
+        // Call siftDown on the reduced heap
+        last = last->prev;
+
+        // Sift down the new root
+        listint_t *largest = *head;
+        listint_t *leftChild = (*head)->next;
+        listint_t *rightChild = (*head)->next->next;
+
+        // Compare root with left child
+        if (leftChild != NULL && leftChild->n > largest->n)
+            largest = leftChild;
+
+        // Compare largest with right child
+        if (rightChild != NULL && rightChild->n > largest->n)
+            largest = rightChild;
+
+        // If the largest element is not the root, then swap and continue sifting down
+        if (largest != *head)
+        {
+            swap(*head, largest);
+            // Print the doubly linked list after each swap
+            listint_t *current = *head;
+            while (current != NULL)
+            {
+                printf("%d ", current->n);
+                current = current->next;
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main(void)
+{
+    listint_t *head = NULL;
+    size_t size = 6;
+
+    // Sample data for the doubly linked list
+    head = createNode(12);
+    head->next = createNode(11);
+    head->next->prev = head;
+    head->next->next = createNode(13);
+    head->next->next->prev = head->next;
+    head->next->next->next = createNode(5);
+    head->next->next->next->prev = head->next->next;
+    head->next->next->next->next = createNode(6);
+    head->next->next->next->next->prev = head->next->next->next;
+    head->next->next->next->next->next = createNode(7);
+    head->next->next->next->next->next->prev = head->next->next->next->next;
+
+    printf("Original Doubly Linked List: ");
+    listint_t *current = head;
+    while (current != NULL)
+    {
+        printf("%d ", current->n);
+        current = current->next;
+    }
+    printf("\n");
+
+    printf("Heap Sort Steps:\n");
+    heap_sort(&head, size);
+
+    printf("Sorted Doubly Linked List: ");
+    current = head;
+    while (current != NULL)
+    {
+        printf("%d ", current->n);
+        current = current->next;
+    }
+    printf("\n");
+
+    // Clean up: free memory allocated for the doubly linked list
+    current = head;
+    while (current != NULL)
+    {
+        listint_t *temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    return 0;
 }
 
